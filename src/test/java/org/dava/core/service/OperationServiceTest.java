733c6633924cd76e17ca3a4f9206bc1917a84eab
Brandon Fowler <brandon@fivefowlers.com>
2023-11-08 16:12:45 -0700

    Info log level on  test

--- src/test/java/org/dava/core/service/OperationServiceTest.java
+++ src/test/java/org/dava/core/service/OperationServiceTest.java
@@ -35,7 +35,7 @@ class OperationServiceTest {
 
     static Database database;
     static int ITERATIONS = 1000;
-    static Level logLevel = Level.DEBUG;
+    static Level logLevel = Level.INFO;
 //    static String DB_ROOT = "/Users/brandon.fowler/Desktop/db";
     static Mode TABLE_MODE = Mode.MANUAL;
     static String DB_ROOT = "db";
